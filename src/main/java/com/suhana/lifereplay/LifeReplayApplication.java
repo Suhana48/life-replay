5c2634e679dfdbc35745c17a9465a2822c0c6cd9
SUHANA <suhanasolanki78@gmail.com>
2026-08-10 22:43:33 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/suhana/lifereplay/LifeReplayApplication.java
@@ -0,0 +1,13 @@
+package com.suhana.lifereplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeReplayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeReplayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/suhana/lifereplay/LifeReplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhana.lifereplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeReplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
